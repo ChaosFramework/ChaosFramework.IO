3dbfe10cf79234d702bbb7c5c1b3c07c2e69948d
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2024-03-25 23:39:55 +0100

    treat non existent directories as empty

--- source/Streams/Sources/FileStreamSource.cs
+++ source/Streams/Sources/FileStreamSource.cs
@@ -13,16 +13,16 @@ namespace ChaosFramework.IO.Streams.Sources
         public FileStreamSource(DirectoryInfo rootDir)
         {
             this.rootDir = rootDir;
-            if (!rootDir.Exists)
-                throw new DirectoryNotFoundException();
         }
 
         IEnumerable<string> StreamSource.EnumerateKeys(string glob)
-            => from file in rootDir.EnumerateFiles("*", SearchOption.AllDirectories)
+            => rootDir.Exists
+             ? from file in rootDir.EnumerateFiles("*", SearchOption.AllDirectories)
                let globRegex = new Regex(GlobRegex.ConvertGlobToRegex(glob), RegexOptions.IgnoreCase)
                let key = file.FullName.Remove(0, rootDir.FullName.Length)
                where globRegex.IsMatch(key)
-               select key;
+               select key
+             : ChaosUtil.Primitives.Array<string>.empty;
 
         bool StreamSource.ContainsKey(string key)
             => File.Exists($"{rootDir.FullName}\\{key}");
