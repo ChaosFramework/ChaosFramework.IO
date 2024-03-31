be000528d488ece2ea7131dd6669a5474afc09ed
InsanityCode <56584762+InsanityCode@users.noreply.github.com>
2024-03-31 15:33:43 +0200

    add documentation for StreamSource interface
    Co-Authored-By: Frame <1663221+FramePerfection@users.noreply.github.com>

--- source/Streams/StreamSource.cs
+++ source/Streams/StreamSource.cs
@@ -5,10 +5,40 @@ namespace ChaosFramework.IO.Streams
 {
     public interface StreamSource
     {
+        /// <summary>
+        ///     Returns whether there is a stream associated with <paramref name="key"/>.
+        ///     Must not throw any exceptions as long as this <see cref="StreamSource"/> is <see cref="alive"/>.
+        ///     If this <see cref="StreamSource"/> is not <see cref="alive"/>, the behavior is undefined.
+        /// </summary>
+        /// <param name="key"> The key to check for. </param>
         bool ContainsKey(string key);
 
+        /// <summary>
+        ///     Returns a stream associated with the provided <paramref name="key"/>.
+        ///     For each call of <see cref="OpenRead(string)"/> a unique stream instance will be retrieved.
+        ///     Only explicitly throws exceptions that derive from <see cref="StreamSourceException"/>.
+        ///     If this <see cref="StreamSource"/> is not <see cref="alive"/>, the behavior is undefined.
+        /// </summary>
+        /// <param name="key"> The key to retrieve a stream for. </param>
+        /// <exception cref="IllegalKeyException">
+        ///     Thrown if the provided <paramref name="key"/> has an illegal format.
+        /// </exception>
+        /// <exception cref="KeyNotFoundException">
+        ///     Thrown if no stream could be associated with <paramref name="key"/>.
+        /// </exception>
+        /// <exception cref="StreamAccessException">
+        ///     Thrown if the stream associated with <paramref name="key"/> was found, but cannot be read.
+        /// </exception>
+        /// <exception cref="StreamSourceException">
+        ///     Thrown if retrieving the stream failed for any other reason.
+        /// </exception>
         Stream OpenRead(string key);
 
+        /// <summary>
+        ///     Yields all keys known to this <see cref="StreamSource"/>.
+        ///     Must not throw any exceptions as long as this <see cref="StreamSource"/> is <see cref="alive"/>.
+        ///     If this <see cref="StreamSource"/> is not <see cref="alive"/>, the behavior is undefined.
+        /// </summary>
         SysCol.IEnumerable<string> EnumerateKeys();
 
         /// <summary>
